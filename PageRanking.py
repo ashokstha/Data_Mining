0bf6b8f7287c3fd0e320b1bdad279e0e57bccbfe
ashok <cs.ashok007@gmail.com>
2018-02-02 17:20:01 -0600

    print statement updated

--- PageRanking.py
+++ PageRanking.py
@@ -5,24 +5,24 @@ import numpy as np
 
 threshold = 1e-5
 
-def pageRanking(G):
+def pageRanking(x):
     # Page ranking function returns page-ranking vector
     pageRanks = np.array([1, 0, 0, 0, 0])
     oldRanks = np.array([0, 0, 0, 0, 0])
 
     while np.sum(np.abs(pageRanks - oldRanks)) > threshold:
         oldRanks = pageRanks
-        pageRanks = np.dot(G,pageRanks)
+        pageRanks = np.dot(x,pageRanks)
 
     return pageRanks
 
 
 if __name__ == "__main__":
     #randomly generate 5x5 matrix
-    x = np.random.random((5, 5))
+    G = np.random.random((5, 5))
     #divide each element of matrix with sum of col values to make sum of col val 1
-    x = x/x.sum(axis=0)
-    print(x)
+    G = G/G.sum(axis=0)
+    print("\nTransitional Probability Matrix:\n" + str(G))
 
-    pageRank = pageRanking(x)
-    print([rank for rank in pageRank])
+    pageRank = pageRanking(G)
+    print("\nPage-Ranking Vector:\n" + str(pageRank))
