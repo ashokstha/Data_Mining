8dbd0110818bc1487b92ecf2993e95b22008b530
akstha007 <cs.ashok007@gmail.com>
2018-03-18 18:30:00 -0500

    Assignement 3 LDA generalization multiplication of each elements with decision function: b1*x1 + b2*x2 + b0 = 0, Here {b0, b1, b2} are co-effiencients(constants), {x1, x2} => i/p variables

--- Assignment3.py
+++ Assignment3.py
@@ -2,10 +2,12 @@
 
 import numpy as np
 import matplotlib
+
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy as np
 import sklearn
+
 sklearn.__version__
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
@@ -20,44 +22,51 @@ def main():
     label[0:100] = 0
     X_in = np.column_stack((x, y))
 
-    mean1_x = np.mean(x[:100], dtype=np.float64)
-    mean2_x = np.mean(x[100:], dtype=np.float64)
-    mean1_y = np.mean(y[:100], dtype=np.float64)
-    mean2_y = np.mean(y[100:], dtype=np.float64)
-
-    mean1 = [mean1_x, mean1_y]
-    mean2 = [mean2_x, mean2_y]
-
-    print(mean1, mean2)
-
-    std1_x = np.std(x[:100], dtype=np.float64)
-    std2_x = np.std(x[100:], dtype=np.float64)
-    std1_y = np.std(y[:100], dtype=np.float64)
-    std2_y = np.std(y[100:], dtype=np.float64)
-
-    std1 = [std1_x, std1_y]
-    std2 = [std2_x, std2_y]
-
-    print(std1, std2)
-    print(np.mean(X_in[100:], axis=0))
-
-
-
+    mean_1 = np.mean(X_in[:100], axis=0)
+    mean_2 = np.mean(X_in[100:], axis=0)
+    print("means: ", mean_1, mean_2)
 
     '''
-    plt.scatter(x, y, c=label)
+    std_1 = np.std(X_in[:100], axis=0)
+    std_2 = np.std(X_in[100:], axis=0)
+    print("std: ", std_1, std_2)
+    '''
+
+    cov_1 = np.cov(X_in[:100].T)
+    cov_2 = np.cov(X_in[100:].T)
+    cov = np.cov(X_in.T)
+    print("Covariance:", cov_1, cov_2, cov)
+
+    inv_cov1 = np.linalg.inv(cov_1)
+    inv_cov2 = np.linalg.inv(cov_2)
+    inv_cov = np.linalg.inv(cov)
+    print("Inverse:", inv_cov1, inv_cov2, inv_cov)
+
+    #mean_1 = [0, 0]
+    #mean_2 = [3, 4]
+    #inv_cov = [[2, -1], [-1, 3]]
+
+    [m11, m12] = mean_1
+    [m21, m22] = mean_2
+
+    [[a, b], [c, d]] = inv_cov
+
+    # co-efficients of decision variable
+    b0 = ((m11 ** 2 - m21 ** 2) * a + (m12 ** 2 - m22 ** 2) * d + (m11 * m12 - m21 * m22) * (b + c))/2
+    b1 = -(2 * (m11 - m21) * a + (m12 - m22) * (b + c))/2
+    b2 = -(2 * (m12 - m22) * d + (m11 - m21) * (b + c))/2
+
+    print("coeffiecients: ", b0, b1, b2)
+
+    # plt.scatter(x, y, c=label)
+    '''
     plt.scatter(mean1_x,mean1_y, marker="*", c="#00FF00")
     plt.scatter(mean2_x, mean2_y, marker="*", c="#0000FF")
     plt.scatter(std1_x, std1_y + mean1_y, marker="^", c="#00FF00")
     plt.scatter(std2_x, std2_y + mean2_y, marker="^", c="#0000FF")
-
-    plt.show()
     '''
 
-    clf = LDA()
-    clf.fit(X_in, label)
-
-    print(clf.predict([[-0.8, -1]]))
+    # plt.show()
 
 
 if __name__ == "__main__":
